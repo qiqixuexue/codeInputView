f694d95e7d08e5ed3b34770f092e1eb1a316e274
YQ <809606297@qq.com>
2016-07-12 20:05:42 +0800

    自定义修改点击后的图片

--- app/src/main/java/com/yq/codeinputview/MainActivity.java
+++ app/src/main/java/com/yq/codeinputview/MainActivity.java
@@ -2,12 +2,32 @@ package com.yq.codeinputview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.yq.codeinputview.view.CodeInputView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements CodeInputView.IPayCode{
+
+    @Bind(R.id.code_input)
+    CodeInputView codeInputView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        ButterKnife.bind(this);
+        codeInputView.setListener(this);
+    }
+
+    @Override
+    public void getPayCode(String value) {
+        Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
     }
 }
